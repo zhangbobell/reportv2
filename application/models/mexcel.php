f822895d9f76f0e1bb2225219b6d1cfbbc2e66d1
winzhaody <winzhaody@gmail.com>
2014-11-13 16:57:10 +0800

    finish the excel uploading part and add the function of viewing excel

--- application/models/mexcel.php
+++ application/models/mexcel.php
@@ -30,14 +30,12 @@ class MExcel extends MY_model {
             $this->set_record("db_madebaokang", $sql, $cell);
             $this->update_crawl_item_sku($cell);
         }
-
-
-
-
     }
 
     public function update_crawl_item_sku($d)
     {
+        $sql0 = "SELECT * FROM `crawl_item_sku` WHERE `itemnum` = ?";
+
         $sql1 = "INSERT INTO `crawl_item_sku` (`tag_refresh_updatetime_price`, `itemnum`, `min_price`)
                 VALUES (?, ?, ?)
                 ON DUPLICATE KEY
@@ -54,16 +52,49 @@ class MExcel extends MY_model {
                 `itemnum` = VALUES(`itemnum`),
                 `min_price_wap` = VALUES(`min_price_wap`)";
 
-        if($d['is_wap'] == 0)
+        $sql3 = "UPDATE `crawl_item_sku` SET `tag_refresh_updatetime_price` = ? , `min_price` = ?
+                WHERE `itemnum` = ?";
+
+        $sql4 = "UPDATE `crawl_item_sku` SET `tag_refresh_updatetime_price` = ? , `min_price_wap` = ?
+                WHERE `itemnum` = ?";
+
+        //echo $this->get_result_array("db_madebaokang", $sql0, );
+        $config= $this->select_DB("db_madebaokang");
+        $this->load->database($config);
+
+        $query = $this->db->query($sql0, $d['itemnum']);
+
+        if($query->num_rows() > 0)
         {
-            $this->set_record("db_madebaokang", $sql1, Array('tag_refresh_updatetime_price'=>$d['updatetime'],
-                'itemnum'=>$d['itemnum'], 'min_price'=>$d['min_price']));
+            if($d['is_wap'] == 0)
+            {
+                $this->set_record("db_madebaokang", $sql3, Array('tag_refresh_updatetime_price'=>$d['updatetime'],
+                 'min_price'=>$d['min_price'], 'itemnum'=>(string)$d['itemnum']));
+            }
+            else
+            {
+                $this->set_record("db_madebaokang", $sql4, Array('tag_refresh_updatetime_price'=>$d['updatetime'],
+                 'min_price_wap'=>$d['min_price'], 'itemnum'=>(string)$d['itemnum']));
+            }
         }
         else
         {
-            $this->set_record("db_madebaokang", $sql2, Array('tag_refresh_updatetime_price'=>$d['updatetime'],
-                'itemnum'=>$d['itemnum'], 'min_price_wap'=>$d['min_price']));
+            if($d['is_wap'] == 0)
+            {
+                $this->set_record("db_madebaokang", $sql1, Array('tag_refresh_updatetime_price'=>$d['updatetime'],
+                    'itemnum'=>$d['itemnum'], 'min_price'=>$d['min_price']));
+            }
+            else
+            {
+                $this->set_record("db_madebaokang", $sql2, Array('tag_refresh_updatetime_price'=>$d['updatetime'],
+                    'itemnum'=>$d['itemnum'], 'min_price_wap'=>$d['min_price']));
+            }
         }
+
+
+
+
+
     }
 
 }
