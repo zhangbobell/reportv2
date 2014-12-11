cb3606e0079aae6419272e768d07204e3f9ca6ba
zhangbobell <zhangbobell@163.com>
2014-12-11 21:41:42 +0800

    modified the sidebar

--- application/views/templates/sidebar_report.php
+++ application/views/templates/sidebar_report.php
@@ -5,6 +5,9 @@
     <li><a href="graph/init_tendency">趋势</a></li>
     <li><a href="graph/init_target">目标管理</a></li>
     <li><a href="graph/init_product">产品渠道分布</a></li>
+    <?php if($this->session->userdata('groupID') == 0): ?>
+        <li><a href="price/control">乱价模块</a></li>
+    <?php endif; ?>
 
     <!--        <li><a href="price/inputTarget/">录入下周目标</a></li>-->
     <!--        <li><a href="price/inputComplete">录入今日完成</a></li>-->
