ff0ce047cc8ee7fdd946ddeb8c89cfa00e88b517
zhangbobell <zhangbobell@163.com>
2014-12-16 12:23:54 +0800

    fixed the upload bugs and upload style

--- application/views/price/header_add_upload.php
+++ application/views/price/header_add_upload.php
@@ -1,2 +1,3 @@
 <link href="<?php echo base_url().CSS_DIR; ?>/uploaderStyle.css" rel="stylesheet" type="text/css" />
 <link href="<?php echo base_url().TP_DIR; ?>/webuploader/webuploader.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url().TP_DIR; ?>/mrjsontable/css/mrjsontable.css" rel="stylesheet" type="text/css" />
