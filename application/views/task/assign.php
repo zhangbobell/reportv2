ab87b1159b9ddc2b07361d1f70b8ba3175712108
unknown <zhangbobell@163.com>
2015-02-03 22:32:15 +0800

    add task module

--- application/views/task/assign.php
+++ application/views/task/assign.php
@@ -3,9 +3,9 @@
     <div class="col-md-12 l mt20">
         <ul class="task-list col-md-2 mt30">
             <?php foreach($task_list as $item):?>
-                <li><a class="acti-item" href="javascript:;" data-id="<?php echo $item['activity_id'];?>"><?php echo $item['activity_name'];?></a></li>
+                <li class="col-md-12"><a class="acti-item" href="javascript:;" data-id="<?php echo $item['activity_id'];?>"><?php echo $item['activity_name'];?></a></li>
             <?php endforeach;?>
-            <div id="abs"></div>
+            <div id="abs" class="abs"></div>
         </ul>
         <div class="col-md-10">
             <div class="col-md-12" id="task-detail"></div>
