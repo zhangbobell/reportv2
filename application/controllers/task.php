2f19c53c5d1c2d80ba85eda2de4e3b291bf11332
liuya2015 <tanjinzhi10@hotmail.com>
2015-03-05 22:13:11 +0800

    test mail page

--- application/controllers/task.php
+++ application/controllers/task.php
@@ -56,8 +56,11 @@ class Task extends CI_Controller {
         $this->load->view('templates/header_task', $data);
         $this->load->view('templates/sliderbar_task');
         $this->load->view('templates/banner_task');
-//        $this->load->view('task/task_mytask');
-        $this->load->view('task/task_taskdetail');
+//        $this->load->view('task/task_home');
+        $this->load->view('task/task_mytask');
+//        $this->load->view('task/task_taskdetail');
+//        $this->load->view('task/task_decisionmail');  // 未完成：需要附加CSS  js
+
         $this->load->view('templates/footer_task');
         $this->load->view('templates/footer_task_script');
         $this->load->view('templates/footer_function_task');
