9c5a2f1106f123af28fcfe5966c87050e8a14baf
liuya2015 <tanjinzhi10@hotmail.com>
2015-03-05 21:19:32 +0800

    修复进度条，欢迎提示

--- application/views/templates/banner_task.php
+++ application/views/templates/banner_task.php
@@ -24,7 +24,7 @@
                         <li>
                             <div class="dropdown-messages-box">
                                 <a href="profile.html" class="pull-left">
-                                    <img alt="image" class="img-circle" src="img/a7.jpg">
+                                    <img alt="image" class="img-circle" src="<?php echo base_url().IMG_DIR;?>/a7.jpg">
                                 </a>
                                 <div class="media-body">
                                     <small class="pull-right">46h ago</small>
@@ -37,7 +37,7 @@
                         <li>
                             <div class="dropdown-messages-box">
                                 <a href="profile.html" class="pull-left">
-                                    <img alt="image" class="img-circle" src="img/a4.jpg">
+                                    <img alt="image" class="img-circle" src="<?php echo base_url().IMG_DIR;?>/a4.jpg">
                                 </a>
                                 <div class="media-body ">
                                     <small class="pull-right text-navy">5h ago</small>
@@ -50,7 +50,7 @@
                         <li>
                             <div class="dropdown-messages-box">
                                 <a href="profile.html" class="pull-left">
-                                    <img alt="image" class="img-circle" src="img/profile.jpg">
+                                    <img alt="image" class="img-circle" src="<?php echo base_url().IMG_DIR;?>/profile.jpg">
                                 </a>
                                 <div class="media-body ">
                                     <small class="pull-right">23h ago</small>
--- application/views/templates/footer_task_script.php
+++ application/views/templates/footer_task_script.php
@@ -1,19 +1,19 @@
 
 <!-- Mainly scripts -->
-<link src="<?php echo base_url().JS_DIR;?>/jquery-2.1.1.js"></script>
-<link src="<?php echo base_url().JS_DIR;?>js/bootstrap.min.js"></script>
-<link src="<?php echo base_url().JS_DIR;?>/plugins/metisMenu/jquery.metisMenu.js"></script>
-<link src="<?php echo base_url().JS_DIR;?>/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+<script src="<?php echo base_url().JS_DIR;?>/jquery-2.1.1.js"></script>
+<script src="<?php echo base_url().JS_DIR;?>/bootstrap.min.js"></script>
+<script src="<?php echo base_url().JS_DIR;?>/plugins/metisMenu/jquery.metisMenu.js"></script>
+<script src="<?php echo base_url().JS_DIR;?>/plugins/slimscroll/jquery.slimscroll.min.js"></script>
 
 <!-- Custom and plugin javascript -->
-<link src="<?php echo base_url().JS_DIR;?>/inspinia.js"></script>
-<link src="<?php echo base_url().JS_DIR;?>/plugins/pace/pace.min.js"></script>
+<script src="<?php echo base_url().JS_DIR;?>/inspinia.js"></script>
+<script src="<?php echo base_url().JS_DIR;?>/plugins/pace/pace.min.js"></script>
 
 <!-- jQuery UI -->
-<link src="<?php echo base_url().JS_DIR;?>/plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="<?php echo base_url().JS_DIR;?>/plugins/jquery-ui/jquery-ui.min.js"></script>
 
 <!-- GITTER -->
-<link src="<?php echo base_url().JS_DIR;?>/plugins/gritter/jquery.gritter.min.js"></script>
+<script src="<?php echo base_url().JS_DIR;?>/plugins/gritter/jquery.gritter.min.js"></script>
 
 <!-- Toastr -->
-<link src="<?php echo base_url().JS_DIR;?>/plugins/toastr/toastr.min.js"></script>
+<script src="<?php echo base_url().JS_DIR;?>/plugins/toastr/toastr.min.js"></script>
